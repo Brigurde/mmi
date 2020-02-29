8b41a49be4ab1040da66001e694dd14200bef99d
Brigurde <48880155+Brigurde@users.noreply.github.com>
2020-02-29 22:12:01 +0100

    fixed <br> when no hour or no minute

--- javascript/moment.js
+++ javascript/moment.js
@@ -27,14 +27,20 @@ var h = Math.floor(seconds % (3600*24) / 3600);
 var m = Math.floor(seconds % 3600 / 60);
 var s = Math.floor(seconds % 60);
 
-var yDisplay = y > 0 ? y + (y == 1 ? " année, " : " années, ") : "";
-var dDisplay = d > 0 ? d + (d == 1 ? " jour, " : " jours, ") : "";
-var hDisplay = h > 0 ? h + (h == 1 ? " heure, " : " heures, ") : "";
-var mDisplay = m > 0 ? m + (m == 1 ? " minute et " : " minutes et ") : "";
+var yDisplay = y > 0 ? y + (y == 1 ? " année, <br>" : " années, <br>") : "";
+var dDisplay = d > 0 ? d + (d == 1 ? " jour, <br>" : " jours, <br>") : "";
+var hDisplay = h > 0 ? h + (h == 1 ? " heure, <br>" : " heures, <br>") : "";
+var mDisplay = m > 0 ? m + (m == 1 ? " minute et <br>" : " minutes et <br>") : "";
 var sDisplay = s > 0 ? s + (s == 1 ? " seconde" : " secondes") : "";
 
+//activer ça pour émuler la fin du compteur
+ // yDisplay = "";
+ // dDisplay ="";
+ // hDisplay = "";
+ // mDisplay ="";
+ // sDisplay = "";
 if(yDisplay+dDisplay+hDisplay+mDisplay+sDisplay == ""){
   return [('<strong>' + 'Enfin ! C\'est le grand jour, le jour de ton départ en retraite !!!!!' + '</strong>'),("")];
 }
-return [('<strong>' + yDisplay + '<br>' + dDisplay + '<br>' + hDisplay + '<br>' + mDisplay + '<br>' + sDisplay+ '</strong>'),(yDisplay+dDisplay+hDisplay+mDisplay+sDisplay)];
+return [('<strong>' + yDisplay + dDisplay + hDisplay + mDisplay + sDisplay+ '</strong>'),(yDisplay+dDisplay+hDisplay+mDisplay+sDisplay)];
 }
